2a1a4ebfc6e575993dde54ba850638a117e9da44
DrPleaseRespect <juliannayr2007@gmail.com>
2022-10-28 17:54:31 +0800

    remove frontend

--- src/main/systemInfo.ts
+++
@@ -1,5 +0,0 @@
-import { cpu, graphics } from 'systeminformation';
-import { lazy } from '../common/util';
-
-export const getGpuInfo = lazy(() => graphics());
-export const getCpuInfo = lazy(() => cpu());
